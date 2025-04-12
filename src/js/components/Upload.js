d610540453a804c6ce3fbcbd6c07519e1f5e722a
Mac Signer <macsigner@gmx.ch>
2025-04-12 17:17:56 +0200

    feat | add basic image upload preview

--- src/js/components/Upload.js
+++ src/js/components/Upload.js
@@ -5,14 +5,22 @@ export default class Upload {
         document.addEventListener('change', delegate('.input--upload [type="file"]', e => {
             const wrapper = e.target.closest('.input');
             const placeholder = wrapper.querySelector('.input__upload-img--placeholder');
+            const imgWrapper = document.createElement('div')
+                || wrapper.querySelector('.input__upload-preview');
+            imgWrapper.classList.add('input__upload-preview');
             const img = wrapper.querySelector('.input__upload-img:not(.input__upload-img--placeholder)')
                 || document.createElement('img');
             img.classList.add('input__upload-img');
             const [file] = e.target.files;
+            imgWrapper.appendChild(img);
 
-            img.src = URL.createObjectURL(file);
+            imgWrapper.innerHTML = `
+                <img class="input__upload-img" src="${URL.createObjectURL(file)}" alt="New avatar image">
+                <button class="button input__upload-reset" type="button">Remove image</button>
+                <label class="button" type="button" for="${e.target.id}">Change image</label>
+            `
 
-            placeholder.after(img);
+            placeholder.after(imgWrapper);
         }));
     }
 }
